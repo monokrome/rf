1c7a61f21d4c5ff0c5943c0c81a7e7b77b6eb573
Bailey Stoner <monokrome@monokro.me>
2017-01-31 15:21:10 -0800

    Clean up error message.

--- rf.c
+++ rf.c
@@ -18,15 +18,12 @@ char* get_directory_name(int argc, char *argv[]) {
 
 int main(int argc, char *argv[]) {
 	short index = 0;
-
 	paths = (char**) malloc(sizeof(char) * MAX_PATHS);
-
 	DIR *directory = opendir(get_directory_name(argc, argv));
-
 	struct dirent *entity;
 
 	if (directory == NULL) {
-		perror("The provided directory does not exist.");
+		perror("");
 		return 1;
 	}
 
@@ -37,8 +34,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	index = arc4random_uniform(index);
-
 	printf("%s\n", paths[index]);
-
 	return 0;
 }
